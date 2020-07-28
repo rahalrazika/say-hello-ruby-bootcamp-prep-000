5fd0eae68e374dc1f8956661fe0c3d8bdf12f53b
Tok Tok <rere055@gmail.com>
2020-07-28 20:32:30 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method heredef say_hello("Gabriela")
 
-def say_hello(name)
-  puts "Hello, #{name}"
+def say_hello("Gabriela")
+  puts "Hello, #{"Gabriela"}"
 end
-say_hello("Gabriela")
+say_hello()
